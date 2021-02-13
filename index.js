1280acc28b603d65d7c409d197aea8fca21497f3
notDavidHenderson <sn2catt@gmail.com>
2021-02-13 01:04:54 +0000

    LOTS OF CATS done

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 // Write your solution here!
-asdfasdfasdflasdjfasdkl;fjasdl;fj
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(){
+  cats.push("Ralph");
+}
+function destructivelyPrependCat(){
+  cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+  cats.length = (cats.length - 1);
+}
+function destructivelyRemoveFirstCat(){
+  cats.shift();
+}
+function appendCat(){
+  const copyCats = [...cats];
+  copyCats.push("Broom");
+  return copyCats;
+}
+function prependCat(){
+  const copyCats = [...cats];
+  copyCats.unshift("Arnold");
+  return copyCats;
+}
+function removeLastCat(){
+  const copyCats = [...cats];
+  copyCats.length = (copyCats.length - 1);
+  return copyCats;
+}
+function removeFirstCat(){
+  const copyCats = [...cats];
+  copyCats.shift();
+  return copyCats;
+}
